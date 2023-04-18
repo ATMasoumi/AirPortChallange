c22ffb494f3ff34c73936e877674025b702b4ec0
Amin <Torabi.dsd@gmail.com>
2023-04-18 18:11:48 +0330

    changing project name spelling

---
+++ AirPortChallenge/AirPortChallenge/AirPortChallenge.h
@@ -0,0 +1,18 @@
+//
+//  AirPortChallenge.h
+//  AirPortChallenge
+//
+//  Created by Amir Masoumi on 4/18/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AirPortChallenge.
+FOUNDATION_EXPORT double AirPortChallengeVersionNumber;
+
+//! Project version string for AirPortChallenge.
+FOUNDATION_EXPORT const unsigned char AirPortChallengeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AirPortChallenge/PublicHeader.h>
+
+
---
+++ AirPortChallenge/AirPortChallengeTests/AirPortChallengeTests.swift
@@ -0,0 +1,16 @@
+//
+//  AirPortChallengeTests.swift
+//  AirPortChallengeTests
+//
+//  Created by Amir Masoumi on 4/18/23.
+//
+
+import XCTest
+import AirPortChallenge
+
+final class AirPortChallengeTests: XCTestCase {
+
+    func testExample() throws {
+       XCTFail("test failed successfully ")
+    }
+}
